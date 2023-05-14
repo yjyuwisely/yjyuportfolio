55bf0373f5e4fa6a74b0a7a36e49bff0f90ec3ea
Yeongjin Yu <124529023+yjyuwisely@users.noreply.github.com>
2023-05-14 18:55:35 +0900

    Changes contents and styles.

--- js/scripts.js
+++ js/scripts.js
@@ -7,68 +7,6 @@
 // Scripts
 // 
 
-//슬라이드쇼
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n); //slideIndex = slideIndex + n 
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  const slides = document.querySelectorAll(".mySlides");
-  const dots = document.querySelectorAll(".dot");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  } else if (n < 1) {
-    slideIndex = slides.length;
-  } else {
-    slideIndex = n;
-  }
-
-  slides.forEach(slide => {
-    slide.style.display = "none";
-  });
-
-  dots.forEach(dot => {
-    dot.classList.remove("active");
-  });
-
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].classList.add("active");
-}
-/*var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}*/
-
 //원본
 window.addEventListener('DOMContentLoaded', event => {
 
